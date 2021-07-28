61913c3c582aa393028ffab5dace29ef66322544
retry78 <retry78@hanmail.net>
2021-07-28 21:45:02 +0900

    Update BJ_10217_2.java

--- BJ_10217_2.java
+++ BJ_10217_2.java
@@ -1,135 +1,80 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-// KCM Travel
-// 다익스트라 + DP 문제
-public class Main {
-	static List<Airport>[] graph;	// 모든 vertex를 담는 리스트
-	static int[][] moneyDp;
-	static final int INF = Integer.MAX_VALUE / 2;
+public class Main{
 	
-	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+	static int n,m;
+	static int[] tree;
+	static int ts = 1; // Index Tree N=1의 Tree 시작점 
+	static int[] num = new int[1000001];
+	
+	public static void main(String args[]) throws IOException{
 		
-		int tc = Integer.valueOf(st.nextToken());
-		StringBuilder sb = new StringBuilder();
-		
-		while(tc --> 0) {
-			st = new StringTokenizer(br.readLine());
-			int n = Integer.valueOf(st.nextToken());
-			int m = Integer.valueOf(st.nextToken());
-			int k = Integer.valueOf(st.nextToken());
-			moneyDp = new int[n + 1][m + 1];	// i : i번 공항까지의 경로, j : 비용 j만큼 소모함, 값 : 가장 작은 시간 (최단시간)   
-			graph = new ArrayList[n + 1];
-			
-			for(int i = 1; i <= n; i++) {
-				graph[i] = new ArrayList<Airport>();
-				Arrays.fill(moneyDp[i], INF);
-			}
-			
-			// 공항 정보 입력받음 
-			for(int i = 0; i < k; i++) {
-				st = new StringTokenizer(br.readLine());
-				int u = Integer.valueOf(st.nextToken());
-				int v = Integer.valueOf(st.nextToken());
-				int c = Integer.valueOf(st.nextToken()); // 비용 
-				int d = Integer.valueOf(st.nextToken()); // 소요시간 
-				graph[u].add(new Airport(v, c, d));
-			}
-			
-			dijkstra(n, m, 1);	// 다익스트라 가즈아잇
-			
-			int min = INF;
-			for(int i = 0; i <= m; i++) { // 가장 최소 시간을 구해보자 
-				min = Math.min(min, moneyDp[n][i]);
-			}
-			
-			if(min == INF) { // 도착하지 못했을 때 
-				sb.append("Poor KCM");
-			}
-			else {
-				sb.append(min);
-			}
-			sb.append("\n");
+		//System.setIn(new FileInputStream("src/BJ_7578.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        n = Integer.parseInt(st.nextToken());
+        
+        // index tree n=1의 tree index 산정 (ts값 설정)
+		while(ts < n){
+			ts *= 2;
 		}
+		tree = new int[ts*2];
 		
-		System.out.println(sb.toString());
+		// A열의 기계 순번을 index tree로 사용하도록 값 입력 받음
+        st = new StringTokenizer(br.readLine());
+        for (int i=1; i<=n; i++){
+        	int a = Integer.parseInt(st.nextToken());
+        	num[a] = i;
+        }
+        
+        long result = 0;
+        st = new StringTokenizer(br.readLine());
+        
+        // B열 기계 순번으로 A열 index를 구하여, index tree update 및 정답 합산
+        for (int i=1; i<=n; i++){
+        	int a = Integer.parseInt(st.nextToken());
+        	update(num[a],1); // 현재 기계번호 순번에 방문여부(1) A열에 표시
+        	result += sum(num[a]+1,n); // A열 index tree에서 이미 방문된 index 수를 합산
+        }
+        
+        bw.write(result+"\n");
+        
+        bw.flush();
+        bw.close();
+        br.close();
+        
 	}
 	
-	// 다익스트라 알고리즘 
-	static void dijkstra(int n, int m, int start) {
-		PriorityQueue<Airport> pq = new PriorityQueue<Airport>();
-		pq.offer(new Airport(start, 0, 0));
-		moneyDp[start][0] = 0;	// 1번 공항까지 0원을 소모했을 때, 최단시간은 0.
-		
-		while(!pq.isEmpty()) {
-			Airport airport = pq.poll();
-			int nowAirport = airport.number;
-			int nowMoney = airport.money;
-			int nowTime = airport.time;
-			
-			if(nowAirport == n) { // 최단 시간에 목적지 도착. 
-				break;
-			}
-			
-			for(Airport air : graph[nowAirport]) {
-				int nextNum = air.number;
-				int nextMoney = air.money + nowMoney;
-				int nextTime = air.time + nowTime;
-				
-				if(nextMoney > m) {
-					continue;
-				}
-				
-				if(moneyDp[nextNum][nextMoney] <= nextTime) {	// 이미 해당 금액으로 해당 번 공항으로 왔을 때의 최소 시간보다 크면 업데이트할 의미가 없다. 
-					continue;
-				}
-				
-				// 불필요한 삽입 방지를 위해 그 이상의 모든 값에 현재 최단시간 값을 설정해준다.
-				// 그래야 위 조건에 부합해서 continue 떠서 넘어가지.
-                			for(int j = nextMoney; j <= m; j++){
-                    				if(moneyDp[nextNum][j] > nextTime) {
-                    					moneyDp[nextNum][j] = nextTime;
-                    				}
-               			 }
-                
-				moneyDp[air.number][nextMoney] = nextTime;
-				pq.offer(new Airport(air.number, nextMoney, nextTime));
-			}
+	static long sum(int idx1,int idx2){
+		int s = ts+idx1-1;
+		int e = ts+idx2-1;
+		long count = 0;
+		while (s<=e){
+            // 시작점이 2개 노드를 포함하지 않으면, 해당 값을 계산에 포함하고 시작점을 +1 해주고,
+            // 종료점이 2개 노드를 포함하지 않으면, 해당 값을 계산에 포함하고 종료점을 -1 해준다.
+			if (s%2 == 1) count += tree[s++];
+			if (e%2 == 0) count += tree[e--];
+			s/=2;
+			e/=2;
+		}
+		return count;
+	}
+	
+	static void update(int idx,int num){
+		int t = ts + idx -1;
+		tree[t] = num;
+		while (t>1){
+			t/=2;
+			tree[t] = tree[t*2] + tree[t*2+1];
 		}
 	}
-}
 
-class Airport implements Comparable<Airport> {
-	int number;
-	int money;
-	int time;
-	
-	
-	Airport(int number, int money, int time) {
-		this.number = number;
-		this.time = time;
-		this.money = money;
-	}
-	
-	@Override
-	public int compareTo(Airport o) {
-		if(this.time < o.time) {
-			return -1;
-		}
-		else if(this.time == o.time) {
-			if(this.money < o.money) {
-				return -1;
-			}
-			return 0;
-		}
-		return 1;
-	}
 }
